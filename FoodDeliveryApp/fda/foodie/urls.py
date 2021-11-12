dc697941e682d81ca81e6888212369095df1db87
ankitmalakar7 <malamalakar786@gmail.com>
2021-11-13 01:55:08 +0530

    on home page

--- FoodDeliveryApp/fda/foodie/urls.py
+++ FoodDeliveryApp/fda/foodie/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="Home"),
+    # path("about/", views.index, name="about"),
+    # path("contact/", views.contact, name="contact"),
+    # path("search/", views.search, name="search"),
+    # path("tracker/", views.tracker, name="tracker"),
+    # path("view/", views.view, name="view"),
+    # path("checkout/", views.checkout, name="checkout"),
 ]
