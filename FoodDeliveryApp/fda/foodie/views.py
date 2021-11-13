be6c97a431d984ae50f204ccaff8e8f710509013
ankitmalakar7 <malamalakar786@gmail.com>
2021-11-14 01:56:44 +0530

    home page almost done

--- FoodDeliveryApp/fda/foodie/views.py
+++ FoodDeliveryApp/fda/foodie/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Foods
+import math as m
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'foodie/index.html')
+    allFoods = []
+    catz_foods = Foods.objects.values('category', 'id')
+    cats = {item['category'] for item in catz_foods}
+    for cat in cats:
+        foods = Foods.objects.filter(category=cat)
+        n = len(foods)
+        nSlides = n // 4 + m.ceil((n / 4) - (n // 4))
+        allFoods.append([foods, range(1, nSlides), nSlides])
+    params = {'allFoods': allFoods}
+    return render(request, "foodie/index.html", params)
